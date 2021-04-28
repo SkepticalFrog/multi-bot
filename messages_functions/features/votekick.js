c5fa68f3c5ad18d9244b789d68b70897d38d7ef5
SkepticalFrog <thegreygringo@gmail.com>
2021-04-28 15:07:32 +0200

    simplified conditions

--- messages_functions/features/votekick.js
+++ messages_functions/features/votekick.js
@@ -5,48 +5,48 @@ const votekick = (message) => {
     message.reply(
       'La syntaxe pour la commande `$votekick` est `$votekick @user`.'
     );
-  } else {
-    const id = args[1].replace(/[!@<>]/gi, '');
-    message.guild.members.fetch(id).then((member) => {
-      if (!member.voice.channel) {
-        message.reply(`<@${id}> n'est pas dans un channel vocal.`);
-        return;
-      }
-      const nbMembers = member.voice.channel.members.size;
-      const inVoiceChannel =
-        member.voice.channel.members.filter((m) => m.id === message.author.id)
-          .size >= 1;
-      if (!inVoiceChannel) {
-        message.reply("Tu ne peux pas kick si tu n'es pas dans le vocal.");
-        return;
-      }
-      const votesNeeded = Math.ceil(nbMembers / 2);
-
-      const filter = (reaction) => {
-        return ['👍'].includes(reaction.emoji.name);
-      };
-
-      message.channel
-        .send(`Pouce en l'air pour kick <@${member.id}>`)
-        .then((m) => {
-          m.react(reaction).then(() => {
-            m.awaitReactions(filter, {
-              max: votesNeeded,
-              time: 60000,
-              errors: ['time'],
-            })
-              .then((collected) => {
-                if (collected.first().count - 1 >= votesNeeded)
-                  kick(member, message);
-                else message.channel.send("Essaie pas de tricher p'tit con.");
-              })
-              .catch(() => {
-                message.reply('Pas assez de votes.');
-              });
-          });
-        });
-    });
+    return;
   }
+  const id = args[1].replace(/[!@<>]/gi, '');
+  message.guild.members.fetch(id).then((member) => {
+    if (!member.voice.channel) {
+      message.reply(`<@${id}> n'est pas dans un channel vocal.`);
+      return;
+    }
+    const nbMembers = member.voice.channel.members.size;
+    const inVoiceChannel =
+      member.voice.channel.members.filter((m) => m.id === message.author.id)
+        .size >= 1;
+    if (!inVoiceChannel) {
+      message.reply("Tu ne peux pas kick si tu n'es pas dans le vocal.");
+      return;
+    }
+    const votesNeeded = Math.ceil(nbMembers / 2);
+
+    const filter = (reaction) => {
+      return ['👍'].includes(reaction.emoji.name);
+    };
+
+    message.channel
+      .send(`Pouce en l'air pour kick <@${member.id}>`)
+      .then((m) => {
+        m.react(reaction).then(() => {
+          m.awaitReactions(filter, {
+            max: votesNeeded,
+            time: 60000,
+            errors: ['time'],
+          })
+            .then((collected) => {
+              if (collected.first().count - 1 >= votesNeeded)
+                kick(member, message);
+              else message.channel.send("Essaie pas de tricher p'tit con.");
+            })
+            .catch(() => {
+              message.reply('Pas assez de votes.');
+            });
+        });
+      });
+  });
 };
 
 const kick = (member, message) => {
