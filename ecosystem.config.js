81ffeabd4ed661e40976ab7bcf7f01316450acd7
SkepticalFrog <thegreygringo@gmail.com>
2021-04-29 22:53:43 +0200

    created pm2 environment

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'multi-bot',
+      script: 'updateAndRun.sh',
+      watch: ['events', 'commands', 'index.js'],
+      watch_delay: 1000,
+      ignore_watch: ['./node_modules', './db', './utils'],
+    },
+  ],
+};
