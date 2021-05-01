b8504822f975070ba95b0eac34e5b7cf40dbbc4b
SkepticalFrog <thegreygringo@gmail.com>
2021-05-01 16:49:30 +0200

    removed useless logs

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -3,14 +3,10 @@ const onMessage = require('./message');
 module.exports = {
   name: 'messageUpdate',
   async execute(oldMessage, newMessage, client) {
-    console.log(`[+] Message has been updated!!`)
-
     if (!newMessage.author.bot) {
       if (newMessage.content !== oldMessage.content) {
         const res = onMessage.execute(newMessage);
-        console.log(`[+] Result of operation is ${res}`)
         if (res === -1) {
-          console.log('[+] Reacting to edited message.');
           newMessage
             .react('834319455450300419')
             .then(() => newMessage.react('👀'))
