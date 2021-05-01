757657474b4d90f9c53c34cbe24589e4de88905f
SkepticalFrog <thegreygringo@gmail.com>
2021-05-01 11:15:32 +0200

    fixed bot reacting to itself

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,15 +2,14 @@ const JSONdb = require('simple-json-db');
 module.exports = {
   name: 'messageUpdate',
   async execute(oldMessage, newMessage, client) {
-    if (newMessage.author.id !== client.id) {
-      if (newMessage.content !== oldMessage.content)
+    if (!newMessage.author.equals(client.user)) {
+      if (newMessage.content !== oldMessage.content) {
         newMessage
           .react('834319455450300419')
           .then(() => newMessage.react('👀'))
           .then(() => newMessage.react('👎'))
           .catch((err) => console.log('[-] Error ==>', err));
-      else newMessage.react('👀');
+      } else newMessage.react('👀');
     }
   },
 };
- 
