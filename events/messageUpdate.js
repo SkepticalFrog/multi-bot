e044fc70d8f90ae33e293799dfa0a34e8fd081c8
SkepticalFrog <thegreygringo@gmail.com>
2021-05-01 08:34:45 +0200

    reactions added for embed

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,20 +1,16 @@
 const JSONdb = require('simple-json-db');
-const Discord = require('discord.js');
-
-const { prefix } = require('../config.json');
-
 module.exports = {
   name: 'messageUpdate',
   async execute(oldMessage, newMessage, client) {
-    if (
-      newMessage.author.id !== client.id &&
-      newMessage.content !== oldMessage.content
-    ) {
-      newMessage
-        .react('834319455450300419')
-        .then(() => newMessage.react('👀'))
-        .then(() => newMessage.react('👎'))
-        .catch((err) => console.log('[-] Error ==>', err));
+    if (newMessage.author.id !== client.id) {
+      if (newMessage.content !== oldMessage.content)
+        newMessage
+          .react('834319455450300419')
+          .then(() => newMessage.react('👀'))
+          .then(() => newMessage.react('👎'))
+          .catch((err) => console.log('[-] Error ==>', err));
+      else newMessage.react('👀');
     }
   },
 };
+ 
