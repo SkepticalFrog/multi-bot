f4cf4f7dd7498f9b75c2e090dedc55ec2af94a6a
SkepticalFrog <thegreygringo@gmail.com>
2021-05-01 12:38:55 +0200

    better reactions

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,13 +2,14 @@ const JSONdb = require('simple-json-db');
 module.exports = {
   name: 'messageUpdate',
   async execute(oldMessage, newMessage, client) {
-    if (!newMessage.author.equals(client.user)) {
+    if (!newMessage.author.bot) {
       if (newMessage.content !== oldMessage.content) {
+        console.log('[+] Reacting to edited message.');
         newMessage
           .react('834319455450300419')
           .then(() => newMessage.react('👀'))
           .then(() => newMessage.react('👎'))
-          .catch((err) => console.log('[-] Error ==>', err));
+          .catch((err) => console.log('[-] Error reacting ==>', err));
       } else newMessage.react('👀');
     }
   },
