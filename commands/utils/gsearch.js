1d14f0fbf3943bdf8a4e473018a369b238463419
SkepticalFrog <thegreygringo@gmail.com>
2021-05-04 12:41:48 +0200

    added axios & lmgtfy search

---
+++ commands/utils/gsearch.js
@@ -0,0 +1,17 @@
+const { default: axios } = require('axios');
+const { GuildEmojiRoleManager } = require('discord.js');
+
+module.exports = {
+  name: 'gsearch',
+  aliases: ['gs', 'google'],
+  description: 'Effectue une recherche sur Google.',
+  cooldown: 10,
+  args: true,
+  permissions: 'ADMINISTRATOR',
+  execute: async (message, args) => {
+    const query = args.join('+');
+    const url = 'https://lmgtfy.com/?q=' + query + '&iie=1';
+
+    message.reply('Laisse moi chercher ça pour toi: ' + url);
+  },
+};
